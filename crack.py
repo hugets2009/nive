a2cb78fad70366c279d44e18f7457c3720444aa4
hugets2009 <111659082+hugets2009@users.noreply.github.com>
2022-08-22 15:25:02 +0300

    Add files via upload

---
+++ crack.py
@@ -0,0 +1,16 @@
+import request
+
+url = "https://rovaniemi.inschool.fi"
+
+credentials = {
+    "usermame","hugo.mattila@roiedu.fi"
+    "password","password",
+    "Login","Login"
+}
+
+#print (password.txt)
+
+if password.txt.find("login failed") == -1:
+    print("Login success")
+else:
+    print("credentials are incorrect")
